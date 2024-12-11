b965797ab0ca5fb1b80edaa938659eac5b2585c4
Emilio-Madrigal <madrigalemi31@gmail.com>
2024-12-10 20:28:15 -0600

    falta terminar los php y conexion a la bd adaptar todo el crud para que lo haga en la bd y no arraylist

--- app/src/main/java/com/example/integrador/modify_user.java
+++ app/src/main/java/com/example/integrador/modify_user.java
@@ -1,54 +1,35 @@
 package com.example.integrador;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TimePicker;
 import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Calendar;
+import com.example.integrador.R;
 
 import global.info;
 import pojo.paciente;
-import pojo.cita;
 
 public class modify_user extends AppCompatActivity {
-    EditText nombre,edad, peso, altura, telefono, hora,fecha;
-    Spinner actividad, meta;
-    Button modificar,anterior,siguiente;
+    EditText nombrem, edadm, pesom, alturam, telefonom, citahoram, citafecham;
+    Spinner actm;
+    Button anterior, modificar, siguiente;
     Integer posicion = -1;
+    Toolbar toolbar;
     String Seleccionactividad;
     SharedPreferences archivo;
-
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
-    cita datos = new cita ();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,31 +37,33 @@ public class modify_user extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_modify_user);
 
-        archivo = this.getSharedPreferences("sesion", Context.MODE_PRIVATE);
-        nombre=findViewById (R.id.nombre);
+        nombrem = findViewById(R.id.nombrem);
+        edadm = findViewById(R.id.edadm);
+        pesom = findViewById(R.id.pesom);
+        alturam = findViewById(R.id.alturam);
+        telefonom = findViewById(R.id.telefonom);
+        citahoram = findViewById(R.id.citahoram);
+        citafecham = findViewById(R.id.citafecham);
+        actm = findViewById(R.id.actm);
+
+        anterior = findViewById(R.id.anterior);
         modificar = findViewById(R.id.Modificar);
-        siguiente=findViewById (R.id.siguiente);
-        anterior=findViewById (R.id.anterior);
-        edad = findViewById(R.id.edad);
-        peso = findViewById(R.id.peso);
-        altura = findViewById(R.id.altura);
-        telefono = findViewById(R.id.telefono);
+        siguiente = findViewById(R.id.siguiente);
 
-        //actividad = findViewById(R.id.spinner_act);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //hora=findViewById (R.id.citahora);
-        //fecha=findViewById (R.id.citafecha);
-
-        actividad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        actm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Seleccionactividad = actividad.getItemAtPosition(i).toString();
+
+                Seleccionactividad = actm.getItemAtPosition(i).toString();
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
-        });
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
 
         anterior.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,132 +78,61 @@ public class modify_user extends AppCompatActivity {
                 actualizar();
             }
         });
+
         siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 siguiente();
             }
         });
-        hora.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                horaC();
-            }
-        });
-        anterior.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                anterior();
-            }
-        });
-        modificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actualizar();
-            }
-        });
-        siguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                siguiente();
-            }
-        });
-        fecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int dia, mes, ayo;
-                Calendar actual=Calendar.getInstance();
-                dia=actual.get(Calendar.DAY_OF_MONTH);
-                mes=actual.get(Calendar.MONTH);
-                ayo=actual.get(Calendar.YEAR);
-
-                DatePickerDialog dialog = new DatePickerDialog(modify_user.this, android.R.style.Theme_Holo_Dialog_MinWidth,
-                        mDateSetListener,ayo,mes,dia);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable (Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-        mDateSetListener= new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int dayofMonth) {
-                datos.dia=dayofMonth;
-                datos.mes=month+1;
-                datos.Year=year;
-                String cadena;
-
-                cadena = " "+datos.dia+"/"+datos.mes+"/"+datos.Year;
-                fecha.setText(cadena);
-            }
-        };
-
-    }
-    private void horaC() {
-        int hr, min;
-        Calendar actual = Calendar.getInstance();
-        hr = actual.get(Calendar.HOUR_OF_DAY);
-        min = actual.get(Calendar.MINUTE);
-        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int hourofday, int minute) {
-                datos.minutos = minute;
-                datos.horas = hourofday;
-                String cadena;
-                cadena = " " + datos.horas + ":" + datos.minutos;
-                hora.setText(cadena);
-            }
-        },hr,min,true);
-        tpd.show();
     }
 
     private void siguiente() {
         Integer tamaño = info.listapaciente.size();
-        if (tamaño == 0) {
-            Toast.makeText(this, "Lista vacía", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        posicion = (posicion + 1) % tamaño; // si sobrepasa vuelve a cero
-        //mostrarEquipo();
+        posicion = (posicion + 1) % tamaño; // Si sobrepasa vuelve a cero
+        mostrarPaciente();
     }
+
     private void actualizar() {
         paciente pacienteActual = info.listapaciente.get(posicion);
-
-        // Actualizar los campos del paciente logueado
-        pacienteActual.setNombre (nombre.getText ().toString ());
-        pacienteActual.setEdad(edad.getText().toString());
-        pacienteActual.setPeso(peso.getText().toString());
-        pacienteActual.setAltura(altura.getText().toString());
+        pacienteActual.setNombre(nombrem.getText().toString());
+        pacienteActual.setEdad(edadm.getText().toString());
+        pacienteActual.setPeso(pesom.getText().toString());
+        pacienteActual.setAltura(alturam.getText().toString());
+        pacienteActual.setTelefono(telefonom.getText().toString());
         pacienteActual.setActividad(Seleccionactividad);
-        pacienteActual.setTelefono(telefono.getText().toString());
-        pacienteActual.setHorac (hora.getText ().toString ());
-        pacienteActual.setFechac (fecha.getText ().toString ());
+        pacienteActual.setHorac(citahoram.getText().toString());
+        pacienteActual.setFechac(citafecham.getText().toString());
 
-
-        Toast.makeText(this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Datos actualizados", Toast.LENGTH_SHORT).show();
     }
+
     private void anterior() {
         Integer tamaño = info.listapaciente.size();
         posicion = (posicion - 1 + tamaño) % tamaño;
-        mostrarpaciente ();
+        mostrarPaciente();
     }
 
-    private void mostrarpaciente() {
+    private void mostrarPaciente() {
         paciente pacienteActual = info.listapaciente.get(posicion);
 
-        // Mostrar los valores actuales en los campos correspondientes
-        edad.setText(pacienteActual.getEdad());
-        peso.setText(pacienteActual.getPeso());
-        altura.setText(pacienteActual.getAltura());
-        telefono.setText(pacienteActual.getTelefono());
+        nombrem.setText(pacienteActual.getNombre());
+        edadm.setText(pacienteActual.getEdad());
+        pesom.setText(pacienteActual.getPeso());
+        alturam.setText(pacienteActual.getAltura());
+        telefonom.setText(pacienteActual.getTelefono());
+        citahoram.setText(pacienteActual.getHorac());
+        citafecham.setText(pacienteActual.getFechac());
 
-
-        // Configurar los valores iniciales de los spinners
-        if (pacienteActual.getActividad() != null) {
-            int actividadPosition = ((android.widget.ArrayAdapter<String>) actividad.getAdapter())
-                    .getPosition(pacienteActual.getActividad());
-            actividad.setSelection(actividadPosition);
+        // Para el Spinner, selecciona el índice correspondiente
+        String actividad = pacienteActual.getActividad();
+        for (int i = 0; i < actm.getCount(); i++) {
+            if (actm.getItemAtPosition(i).toString().equals(actividad)) {
+                actm.setSelection(i);
+                break;
+            }
         }
-
     }
     @Override
     public void onOptionsMenuClosed(Menu menu) {
